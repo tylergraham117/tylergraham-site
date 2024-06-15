402e97ec095d6f4aac98d073f53831918873e8cf
tylergraham117 <tylerfgraham117@gmail.com>
2024-06-15 17:06:26 -0500

    Quartz sync: Jun 15, 2024, 5:06 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -19,6 +19,9 @@ export default ((opts?: Options) => {
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href={link}>{text}</a>
+            </li>
           ))}
         </ul>
       </footer>

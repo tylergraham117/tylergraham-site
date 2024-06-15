31ffbe29c462780897853e6511a1f9c5cf46cec8
tylergraham117 <tylerfgraham117@gmail.com>
2024-06-15 18:04:52 -0500

    Quartz sync: Jun 15, 2024, 6:04 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -20,7 +20,30 @@ export default (() => {
 
     return (
       <head>
-
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
+          <>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
+          </>
+        )}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
+        <meta property="og:width" content="1200" />
+        <meta property="og:height" content="675" />
+        <link rel="icon" href={iconPath} />
+        <meta name="description" content={description} />
+        <meta name="generator" content="Quartz" />
+        {css.map((href) => (
+          <link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve />
+        ))}
+        {js
+          .filter((resource) => resource.loadTime === "beforeDOMReady")
+          .map((res) => JSResourceToScriptElement(res, true))}
       </head>
     )
   }
